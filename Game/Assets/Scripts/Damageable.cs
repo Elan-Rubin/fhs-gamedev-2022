1c1b5818fac6617ab05adcd0bb0fa151eab2cce4
trainb0y <trainb0ydev@gmail.com>
2022-09-13 16:54:14 -0600

    Beginnings of a health bar

--- Game/Assets/Scripts/Damageable.cs
+++ Game/Assets/Scripts/Damageable.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 /// <summary>
 /// Represents something that can be damaged. Keeps track of <see cref="Health"/>
-/// </summary>
+/// </summary>?
 public class Damageable : MonoBehaviour
 {
     private int _health; // backing field for Health (can mostly be ignored)
+    public HealthBar bar; // health bar, if it exists
 
     /// <summary>
     /// The current amount of health
     /// Automatically clamped between 0 and <see cref="maxHealth"/>
-    /// </summary>
+    /// </summary>_tr
     public int Health
     {
         get
         {
             return _health;
         }
-        private set // don't let others modify health directly, but anyone can read it
+        private set // don't let others modify healtpublic HealGameObject prefab;h directly, but anyone can read it
         {
             _health = Mathf.Clamp(value, 0, maxHealth); // clamp health within 0 and max
         }
@@ -26,7 +27,7 @@ public class Damageable : MonoBehaviour
     /// <summary>
     /// The maximum health this can hold
     /// </summary>
-    public int maxHealth;
+    public int maxHealth { get; private set; } // don't allow others to set this
 
     
     /// <summary>
@@ -39,6 +40,10 @@ public class Damageable : MonoBehaviour
         var oldHealth = Health;
         // this is where stuff like armor and buffs might go?
         Health -= damage;
+
+        // update the health bar, if it exists
+        if (bar != null) bar.updateBar();
+
         return oldHealth - Health;
     }
 }
