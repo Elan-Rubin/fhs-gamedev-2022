4663de8e53b5a23a36e07769d96ecae57d69a329
Cr1ps0n <Cripsongames@gmail.com>
2022-09-27 17:07:53 -0600

    Update PlayerMovement.cs
    Added a second jump.

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rigidBody;
     public float jumpdistance = 1;
     bool canJump = true;
+    public int baseJumpsRemaining = 2;
+    public int jumpsRemaining;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -18,10 +20,16 @@ public class PlayerMovement : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up,jumpdistance);
         Debug.Log(hit.collider);
-        if (UnityEngine.Input.GetAxisRaw("Vertical")>0.1f && hit.collider!=null && canJump == true)
+        if (hit.collider != null)
         {
+            jumpsRemaining = baseJumpsRemaining;
+        }
+        if (UnityEngine.Input.GetAxisRaw("Vertical")>0.1f && canJump == true && jumpsRemaining > 0)
+        {
+            jumpsRemaining --;
             StartCoroutine(JumpDelay());
-            rigidBody.velocity += Vector2.up * jumpHeight;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpHeight);
+            //rigidBody.velocity += Vector2.up * jumpHeight;
         }
         rigidBody.velocity = new Vector2(runSpeed * UnityEngine.Input.GetAxisRaw("Horizontal"), rigidBody.velocity.y);
         //rigidBody.velocity += Vector2.right * runSpeed * UnityEngine.Input.GetAxisRaw("Horizontal");
