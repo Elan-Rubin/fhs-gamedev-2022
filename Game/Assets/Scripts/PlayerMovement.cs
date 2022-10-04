dac26ed7962ec2ee45c394e6781f6eda11751f2f
Joseph Lev-Tov <jtlevtov@gmail.com>
2022-10-04 16:49:37 -0600

    Finish wall jump
    Finish wall jump, add ground layer

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -11,12 +13,15 @@ public class PlayerMovement : MonoBehaviour
     public float jumpdistance = 1;
     bool canJump = true;
 
+    [SerializeField] private LayerMask ground; // editor ground layer
+    private int groundLayer; // log base 2 of ground (^) actually used
+
     [SerializeField] private float wallJumpDist = 1f; // Distance from side of player
-    [SerializeField] private float wallJumpDistSide = 2f;
+    [SerializeField] private float wallJumpDistSide = 2f; // Side power of wall jump
     private float wallJumpSideDistNow = 0f;
     private bool inAir;
-    void Start()
-    {
+    void Start() {
+        groundLayer = (int)Mathf.Log(ground, 2);
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -25,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up,jumpdistance);
         if (hit.collider == null) {
             inAir = true;
-        }
+        }   
         else {
             inAir = false;
         }
@@ -36,14 +41,16 @@ public class PlayerMovement : MonoBehaviour
             rigidBody.velocity += Vector2.up * jumpHeight;
         }
 
-        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, wallJumpDist);
-        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, wallJumpDist);
+        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, wallJumpDist, ground);
+        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, wallJumpDist, ground);
+        
+        
 
-        if ((left.collider != null || right.collider != null) && Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0 && Input.GetButtonDown("Vertical") && canJump) {
+        if ((left.collider != null || right.collider != null) && /*Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0 &&*/ Input.GetButtonDown("Vertical") && canJump) {
             StartCoroutine(JumpDelay());
             rigidBody.velocity += Vector2.up * jumpHeight;
             wallJumpSideDistNow = left.collider != null ? (wallJumpDistSide) : (wallJumpDistSide * -1);
-            Debug.Log(wallJumpSideDistNow);
+            // Debug.Log(wallJumpSideDistNow);
         }
 
         rigidBody.velocity = new Vector2(runSpeed * UnityEngine.Input.GetAxisRaw("Horizontal") + wallJumpSideDistNow, rigidBody.velocity.y);
@@ -56,5 +63,10 @@ public class PlayerMovement : MonoBehaviour
         canJump = false;
         yield return new WaitForSeconds(0.1f);
         canJump = true;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, wallJumpDist);
+        Gizmos.DrawWireSphere(transform.position, jumpdistance);
     }
 }
