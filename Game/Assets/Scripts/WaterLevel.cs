df96e7d7a273217170255bea9915462ff5a6bea2
Leo Norton <leoznorton@gmail.com>
2023-01-10 19:36:59 -0700

    Gave the player iFrames, Added frog death image, enemies decrease water level

--- Game/Assets/Scripts/WaterLevel.cs
+++ Game/Assets/Scripts/WaterLevel.cs
@@ -7,11 +7,14 @@ public class WaterLevel : MonoBehaviour
 {
     public float waterAmount = 100;
     public float dryingRate = 1;
+    public float iFrameTime = 3f;
     [SerializeField] Slider healthBarSlider;
     [SerializeField] Image indicatorUi;
     [SerializeField] List<Sprite> dropletSprites;
+    [SerializeField] ParticleSystem splashParticles;
     private float maxWaterAmount;
     private bool isInLake = false;
+    private bool isInvincible = false; //I-frames
 
     private void Start()
     {
@@ -57,6 +60,36 @@ public class WaterLevel : MonoBehaviour
     {
         if (collision.GetComponent<Lake>() != null)
         {
-            isInLake = false;        }
+            isInLake = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Enemy"))
+        {
+            DecreaseWaterLevel(10);
+        }
+        if (collision.tag.Equals("Respawn"))
+        {
+            waterAmount = 0;
+        }
+    }
+
+    public void DecreaseWaterLevel(float damage)
+    {
+        if (isInvincible == false)
+        {
+            StartCoroutine(iFrameDelay());
+            waterAmount -= damage;
+            splashParticles.Emit(5);
+        }
+    }
+
+    IEnumerator iFrameDelay()
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(iFrameTime);
+        isInvincible = false;
     }
 }
